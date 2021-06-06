a86b302f1a1e7dc1bbc138c7c22ae23d3eedf906
Exohood <82610137+exohood@users.noreply.github.com>
2021-06-06 10:18:51 +0200

    Add files via upload

---
+++ server/index-node/src/resolver.rs
@@ -0,0 +1,260 @@
+use std::collections::HashMap;
+
+use graph::data::subgraph::status;
+use graph::prelude::*;
+use graph::{
+    components::store::StatusStore,
+    data::graphql::{IntoValue, ObjectOrInterface, ValueMap},
+};
+use graph_graphql::prelude::{ExecutionContext, Resolver};
+use std::convert::TryInto;
+use web3::types::{Address, H256};
+
+/// Resolver for the index node GraphQL API.
+pub struct IndexNodeResolver<R, S> {
+    logger: Logger,
+    graphql_runner: Arc<R>,
+    store: Arc<S>,
+}
+
+impl<R, S> IndexNodeResolver<R, S>
+where
+    R: GraphQlRunner,
+    S: StatusStore,
+{
+    pub fn new(logger: &Logger, graphql_runner: Arc<R>, store: Arc<S>) -> Self {
+        let logger = logger.new(o!("component" => "IndexNodeResolver"));
+        Self {
+            logger,
+            graphql_runner,
+            store,
+        }
+    }
+
+    fn resolve_indexing_statuses(
+        &self,
+        arguments: &HashMap<&String, q::Value>,
+    ) -> Result<q::Value, QueryExecutionError> {
+        let deployments = arguments
+            .get(&String::from("subgraphs"))
+            .map(|value| match value {
+                q::Value::List(ids) => ids
+                    .into_iter()
+                    .map(|id| match id {
+                        s::Value::String(s) => s.clone(),
+                        _ => unreachable!(),
+                    })
+                    .collect(),
+                _ => unreachable!(),
+            })
+            .unwrap_or_else(|| Vec::new());
+
+        let infos = self
+            .store
+            .status(status::Filter::Deployments(deployments))?;
+        Ok(infos.into_value())
+    }
+
+    fn resolve_indexing_statuses_for_subgraph_name(
+        &self,
+        arguments: &HashMap<&String, q::Value>,
+    ) -> Result<q::Value, QueryExecutionError> {
+        // Get the subgraph name from the arguments; we can safely use `expect` here
+        // because the argument will already have been validated prior to the resolver
+        // being called
+        let subgraph_name = arguments
+            .get_required::<String>("subgraphName")
+            .expect("subgraphName not provided");
+
+        debug!(
+            self.logger,
+            "Resolve indexing statuses for subgraph name";
+            "name" => &subgraph_name
+        );
+
+        let infos = self
+            .store
+            .status(status::Filter::SubgraphName(subgraph_name))?;
+
+        Ok(infos.into_value())
+    }
+
+    fn resolve_proof_of_indexing(
+        &self,
+        argument_values: &HashMap<&String, q::Value>,
+    ) -> Result<q::Value, QueryExecutionError> {
+        let deployment_id = argument_values
+            .get_required::<DeploymentHash>("subgraph")
+            .expect("Valid subgraphId required");
+
+        let block_number: u64 = argument_values
+            .get_required::<u64>("blockNumber")
+            .expect("Valid blockNumber required")
+            .try_into()
+            .unwrap();
+
+        let block_hash = argument_values
+            .get_required::<H256>("blockHash")
+            .expect("Valid blockHash required")
+            .try_into()
+            .unwrap();
+
+        let block = BlockPtr::from((block_hash, block_number));
+
+        let indexer = argument_values
+            .get_optional::<Address>("indexer")
+            .expect("Invalid indexer");
+
+        let poi_fut =
+            self.store
+                .clone()
+                .get_proof_of_indexing(&deployment_id, &indexer, block.clone());
+        let poi = match futures::executor::block_on(poi_fut) {
+            Ok(Some(poi)) => q::Value::String(format!("0x{}", hex::encode(&poi))),
+            Ok(None) => q::Value::Null,
+            Err(e) => {
+                error!(
+                    self.logger,
+                    "Failed to query proof of indexing";
+                    "subgraph" => deployment_id,
+                    "block" => format!("{}", block),
+                    "error" => format!("{:?}", e)
+                );
+                q::Value::Null
+            }
+        };
+
+        Ok(poi)
+    }
+
+    fn resolve_indexing_status_for_version(
+        &self,
+        arguments: &HashMap<&String, q::Value>,
+
+        // If `true` return the current version, if `false` return the pending version.
+        current_version: bool,
+    ) -> Result<q::Value, QueryExecutionError> {
+        // We can safely unwrap because the argument is non-nullable and has been validated.
+        let subgraph_name = arguments.get_required::<String>("subgraphName").unwrap();
+
+        debug!(
+            self.logger,
+            "Resolve indexing status for subgraph name";
+            "name" => &subgraph_name,
+            "current_version" => current_version,
+        );
+
+        let infos = self.store.status(status::Filter::SubgraphVersion(
+            subgraph_name,
+            current_version,
+        ))?;
+
+        Ok(infos
+            .into_iter()
+            .next()
+            .map(|info| info.into_value())
+            .unwrap_or(q::Value::Null))
+    }
+}
+
+impl<R, S> Clone for IndexNodeResolver<R, S>
+where
+    R: GraphQlRunner,
+    S: SubgraphStore,
+{
+    fn clone(&self) -> Self {
+        Self {
+            logger: self.logger.clone(),
+            graphql_runner: self.graphql_runner.clone(),
+            store: self.store.clone(),
+        }
+    }
+}
+
+impl<R, S> Resolver for IndexNodeResolver<R, S>
+where
+    R: GraphQlRunner,
+    S: StatusStore,
+{
+    const CACHEABLE: bool = false;
+
+    fn prefetch(
+        &self,
+        _: &ExecutionContext<Self>,
+        _: &q::SelectionSet,
+    ) -> Result<Option<q::Value>, Vec<QueryExecutionError>> {
+        Ok(None)
+    }
+
+    /// Resolves a scalar value for a given scalar type.
+    fn resolve_scalar_value(
+        &self,
+        parent_object_type: &s::ObjectType,
+        field: &q::Field,
+        scalar_type: &s::ScalarType,
+        value: Option<q::Value>,
+        argument_values: &HashMap<&String, q::Value>,
+    ) -> Result<q::Value, QueryExecutionError> {
+        // Check if we are resolving the proofOfIndexing bytes
+        if &parent_object_type.name == "Query"
+            && &field.name == "proofOfIndexing"
+            && &scalar_type.name == "Bytes"
+        {
+            return self.resolve_proof_of_indexing(argument_values);
+        }
+
+        // Fallback to the same as is in the default trait implementation. There
+        // is no way to call back into the default implementation for the trait.
+        // So, note that this is duplicated.
+        // See also c2112309-44fd-4a84-92a0-5a651e6ed548
+        Ok(value.unwrap_or(q::Value::Null))
+    }
+
+    fn resolve_objects(
+        &self,
+        prefetched_objects: Option<q::Value>,
+        field: &q::Field,
+        _field_definition: &s::Field,
+        object_type: ObjectOrInterface<'_>,
+        arguments: &HashMap<&String, q::Value>,
+    ) -> Result<q::Value, QueryExecutionError> {
+        match (prefetched_objects, object_type.name(), field.name.as_str()) {
+            // The top-level `indexingStatuses` field
+            (None, "SubgraphIndexingStatus", "indexingStatuses") => {
+                self.resolve_indexing_statuses(arguments)
+            }
+
+            // The top-level `indexingStatusesForSubgraphName` field
+            (None, "SubgraphIndexingStatus", "indexingStatusesForSubgraphName") => {
+                self.resolve_indexing_statuses_for_subgraph_name(arguments)
+            }
+
+            // Resolve fields of `Object` values (e.g. the `chains` field of `ChainIndexingStatus`)
+            (value, _, _) => Ok(value.unwrap_or(q::Value::Null)),
+        }
+    }
+
+    fn resolve_object(
+        &self,
+        prefetched_object: Option<q::Value>,
+        field: &q::Field,
+        _field_definition: &s::Field,
+        _object_type: ObjectOrInterface<'_>,
+        arguments: &HashMap<&String, q::Value>,
+    ) -> Result<q::Value, QueryExecutionError> {
+        match (prefetched_object, field.name.as_str()) {
+            // The top-level `indexingStatusForCurrentVersion` field
+            (None, "indexingStatusForCurrentVersion") => {
+                self.resolve_indexing_status_for_version(arguments, true)
+            }
+
+            // The top-level `indexingStatusForPendingVersion` field
+            (None, "indexingStatusForPendingVersion") => {
+                self.resolve_indexing_status_for_version(arguments, false)
+            }
+
+            // Resolve fields of `Object` values (e.g. the `latestBlock` field of `EthereumBlock`)
+            (value, _) => Ok(value.unwrap_or(q::Value::Null)),
+        }
+    }
+}
---
+++ server/index-node/src/service.rs
@@ -0,0 +1,268 @@
+use http::header::{
+    self, ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS, ACCESS_CONTROL_ALLOW_ORIGIN,
+    CONTENT_TYPE, LOCATION,
+};
+use hyper::service::Service;
+use hyper::{Body, Method, Request, Response, StatusCode};
+use std::task::Context;
+use std::task::Poll;
+
+use graph::{components::server::query::GraphQLServerError, data::query::QueryResults};
+use graph::{components::store::StatusStore, prelude::*};
+use graph_graphql::prelude::{execute_query, Query as PreparedQuery, QueryExecutionOptions};
+
+use crate::explorer::Explorer;
+use crate::request::IndexNodeRequest;
+use crate::resolver::IndexNodeResolver;
+use crate::schema::SCHEMA;
+
+/// An asynchronous response to a GraphQL request.
+pub type IndexNodeServiceResponse = DynTryFuture<'static, Response<Body>, GraphQLServerError>;
+
+/// A Hyper Service that serves GraphQL over a POST / endpoint.
+#[derive(Debug)]
+pub struct IndexNodeService<Q, S> {
+    logger: Logger,
+    graphql_runner: Arc<Q>,
+    store: Arc<S>,
+    explorer: Arc<Explorer<S>>,
+}
+
+impl<Q, S> Clone for IndexNodeService<Q, S> {
+    fn clone(&self) -> Self {
+        Self {
+            logger: self.logger.clone(),
+            graphql_runner: self.graphql_runner.clone(),
+            store: self.store.clone(),
+            explorer: self.explorer.clone(),
+        }
+    }
+}
+
+impl<Q, S> CheapClone for IndexNodeService<Q, S> {}
+
+impl<Q, S> IndexNodeService<Q, S>
+where
+    Q: GraphQlRunner,
+    S: StatusStore,
+{
+    /// Creates a new GraphQL service.
+    pub fn new(logger: Logger, graphql_runner: Arc<Q>, store: Arc<S>) -> Self {
+        let explorer = Arc::new(Explorer::new(store.clone()));
+
+        IndexNodeService {
+            logger,
+            graphql_runner,
+            store,
+            explorer,
+        }
+    }
+
+    fn graphiql_html() -> &'static str {
+        include_str!("../assets/index.html")
+    }
+
+    /// Serves a static file.
+    fn serve_file(contents: &'static str, content_type: &'static str) -> Response<Body> {
+        Response::builder()
+            .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+            .header(CONTENT_TYPE, content_type)
+            .status(200)
+            .body(Body::from(contents))
+            .unwrap()
+    }
+
+    fn index() -> Response<Body> {
+        Response::builder()
+            .status(200)
+            .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+            .header(CONTENT_TYPE, "text/html")
+            .body(Body::from("OK"))
+            .unwrap()
+    }
+
+    fn handle_graphiql() -> Response<Body> {
+        Self::serve_file(Self::graphiql_html(), "text/html")
+    }
+
+    async fn handle_graphql_query(
+        &self,
+        request_body: Body,
+    ) -> Result<Response<Body>, GraphQLServerError> {
+        let store = self.store.clone();
+        let graphql_runner = self.graphql_runner.clone();
+
+        // Obtain the schema for the index node GraphQL API
+        let schema = SCHEMA.clone();
+
+        let body = hyper::body::to_bytes(request_body)
+            .map_err(|_| GraphQLServerError::InternalError("Failed to read request body".into()))
+            .await?;
+
+        let query = IndexNodeRequest::new(body).compat().await?;
+        let query = match PreparedQuery::new(&self.logger, schema, None, query, None, 100) {
+            Ok(query) => query,
+            Err(e) => return Ok(QueryResults::from(QueryResult::from(e)).as_http_response()),
+        };
+
+        let graphql_runner = graphql_runner.clone();
+        let load_manager = graphql_runner.load_manager();
+
+        // Run the query using the index node resolver
+        let query_clone = query.cheap_clone();
+        let logger = self.logger.cheap_clone();
+        let result = {
+            let options = QueryExecutionOptions {
+                resolver: IndexNodeResolver::new(&logger, graphql_runner, store),
+                deadline: None,
+                max_first: std::u32::MAX,
+                max_skip: std::u32::MAX,
+                load_manager,
+            };
+            let result = execute_query(query_clone.cheap_clone(), None, None, options, false).await;
+            query_clone.log_execution(0);
+            QueryResult::from(
+                // Index status queries are not cacheable, so we may unwrap this.
+                Arc::try_unwrap(result).unwrap(),
+            )
+        };
+
+        Ok(QueryResults::from(result).as_http_response())
+    }
+
+    // Handles OPTIONS requests
+    fn handle_graphql_options(_request: Request<Body>) -> Response<Body> {
+        Response::builder()
+            .status(200)
+            .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+            .header(CONTENT_TYPE, "text/plain")
+            .header(ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, User-Agent")
+            .header(ACCESS_CONTROL_ALLOW_METHODS, "GET, OPTIONS, POST")
+            .body(Body::from(""))
+            .unwrap()
+    }
+
+    /// Handles 302 redirects
+    fn handle_temp_redirect(destination: &str) -> Result<Response<Body>, GraphQLServerError> {
+        header::HeaderValue::from_str(destination)
+            .map_err(|_| {
+                GraphQLServerError::ClientError("invalid characters in redirect URL".into())
+            })
+            .map(|loc_header_val| {
+                Response::builder()
+                    .status(StatusCode::FOUND)
+                    .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+                    .header(CONTENT_TYPE, "text/plain")
+                    .header(LOCATION, loc_header_val)
+                    .body(Body::from("Redirecting..."))
+                    .unwrap()
+            })
+    }
+
+    /// Handles 404s.
+    pub(crate) fn handle_not_found() -> Response<Body> {
+        Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+            .header(CONTENT_TYPE, "text/plain")
+            .body(Body::from("Not found\n"))
+            .unwrap()
+    }
+
+    async fn handle_call(self, req: Request<Body>) -> Result<Response<Body>, GraphQLServerError> {
+        let method = req.method().clone();
+
+        let path = req.uri().path().to_owned();
+        let path_segments = {
+            let mut segments = path.split('/');
+
+            // Remove leading '/'
+            assert_eq!(segments.next(), Some(""));
+
+            segments.collect::<Vec<_>>()
+        };
+
+        match (method, path_segments.as_slice()) {
+            (Method::GET, [""]) => Ok(Self::index()),
+            (Method::GET, ["graphiql.css"]) => Ok(Self::serve_file(
+                include_str!("../assets/graphiql.css"),
+                "text/css",
+            )),
+            (Method::GET, ["graphiql.min.js"]) => Ok(Self::serve_file(
+                include_str!("../assets/graphiql.min.js"),
+                "text/javascript",
+            )),
+
+            (Method::GET, path @ ["graphql"]) => {
+                let dest = format!("/{}/playground", path.join("/"));
+                Self::handle_temp_redirect(&dest)
+            }
+            (Method::GET, ["graphql", "playground"]) => Ok(Self::handle_graphiql()),
+
+            (Method::POST, ["graphql"]) => self.handle_graphql_query(req.into_body()).await,
+            (Method::OPTIONS, ["graphql"]) => Ok(Self::handle_graphql_options(req)),
+
+            (Method::GET, ["explorer", rest @ ..]) => self.explorer.handle(&self.logger, rest),
+
+            _ => Ok(Self::handle_not_found()),
+        }
+    }
+}
+
+impl<Q, S> Service<Request<Body>> for IndexNodeService<Q, S>
+where
+    Q: GraphQlRunner,
+    S: StatusStore,
+{
+    type Response = Response<Body>;
+    type Error = GraphQLServerError;
+    type Future = IndexNodeServiceResponse;
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        let logger = self.logger.clone();
+
+        // Returning Err here will prevent the client from receiving any response.
+        // Instead, we generate a Response with an error code and return Ok
+        Box::pin(
+            self.cheap_clone()
+                .handle_call(req)
+                .map(move |result| match result {
+                    Ok(response) => Ok(response),
+                    Err(err @ GraphQLServerError::ClientError(_)) => {
+                        debug!(logger, "IndexNodeService call failed: {}", err);
+
+                        Ok(Response::builder()
+                            .status(400)
+                            .header(CONTENT_TYPE, "text/plain")
+                            .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+                            .body(Body::from(format!("Invalid request: {}", err)))
+                            .unwrap())
+                    }
+                    Err(err @ GraphQLServerError::QueryError(_)) => {
+                        error!(logger, "IndexNodeService call failed: {}", err);
+
+                        Ok(Response::builder()
+                            .status(400)
+                            .header(CONTENT_TYPE, "text/plain")
+                            .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+                            .body(Body::from(format!("Query error: {}", err)))
+                            .unwrap())
+                    }
+                    Err(err @ GraphQLServerError::InternalError(_)) => {
+                        error!(logger, "IndexNodeService call failed: {}", err);
+
+                        Ok(Response::builder()
+                            .status(500)
+                            .header(CONTENT_TYPE, "text/plain")
+                            .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+                            .body(Body::from(format!("Internal server error: {}", err)))
+                            .unwrap())
+                    }
+                }),
+        )
+    }
+}
